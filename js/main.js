ca4561d818a013439b90551297bcf1d9085f1281
Melissa Wilcox <melissa@emdubb.co>
2016-10-17 15:47:02 -0700

    add convert image to canvas and download

--- js/main.js
+++ js/main.js
@@ -6,13 +6,40 @@ $(document).ready(function(){
     var reader = new FileReader();
 
     reader.onload = function(event) {
-      console.log("test", event.target.result);
+      var name = file.name
       var url = event.target.result;
       var img = new Image();
       img.src = url;
       img.width = img.width / 2
       img.height = img.height / 2
-      console.log("new image!", img.width, img.height)
+
+      var canvas = document.getElementById("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      var ctx = canvas.getContext("2d");
+      // var canvasImage =
+      ctx.drawImage(img, 0, 0, img.width, img.height)
+      // console.log("canvas?", canvas)
+      canvas.toBlob(function(blob) {
+        var zip = new JSZip();
+        zip
+          .folder("export")
+          .file(name, blob);
+        zip
+          .generateAsync({type:"blob"})
+          .then(function(content){
+            saveAs(content, "export.zip");
+          })
+
+
+        // saveAs(blob, name);
+      });
+      // var zip = new JSZip();
+      // zip.file("name.png", img);
+      // zip.generateAsync({type:"blob"})
+      //   .then(function(content){
+      //     saveAs(content, "export.zip");
+      //   })
     }
 
     reader.readAsDataURL(file);
