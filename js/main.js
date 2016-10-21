7dc75a31d38e33912016c5cc7afbb9458aca194f
Melissa Wilcox <melissa@emdubb.co>
2016-10-21 13:59:39 -0700

    add file type restriction

--- js/main.js
+++ js/main.js
@@ -11,34 +11,41 @@ $(document).ready(function(){
   })
 
   function formatFiles(that, callback){
+    $("#file-type-error").hide();
     var files = that.files
+    console.log(files)
     for (var i = 0; i < files.length; i++) {
       (function(file) {
-        var reader = new FileReader();
+        console.log(file.type)
+        if (file.type === "image/png" || file.type === "image/jpeg" || file.type === "image/svg+xml") {
+          var reader = new FileReader();
 
-        reader.onload = function(event) {
-          var name = file.name
-          var url = event.target.result;
+          reader.onload = function(event) {
+            var name = file.name
+            var url = event.target.result;
 
-          var img = new Image();
-          img.src = url;
-          img.width = img.width / 2
-          img.height = img.height / 2
+            var img = new Image();
+            img.src = url;
+            img.width = img.width / 2
+            img.height = img.height / 2
 
-          var canvas = document.getElementById("canvas");
-          canvas.width = img.width;
-          canvas.height = img.height;
-          var ctx = canvas.getContext("2d");
-          ctx.drawImage(img, 0, 0, img.width, img.height)
+            var canvas = document.getElementById("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, img.width, img.height)
 
-          canvas.toBlob(function(blob){
-            zip.file("export/" + name, blob);
-            canvasArray.push(blob)
-            if (canvasArray.length === files.length) {
-              callback && callback();
-            }
-          })
+            canvas.toBlob(function(blob){
+              zip.file("export/" + name, blob);
+              canvasArray.push(blob)
+              if (canvasArray.length === files.length) {
+                callback && callback();
+              }
+            })
 
+          }
+        } else {
+          $("#file-type-error").show();
         }
         reader.readAsDataURL(file);
       })(files[i]);
@@ -49,6 +56,7 @@ $(document).ready(function(){
     zip.generateAsync({type:"blob"})
       .then(function(content) {
           saveAs(content, "export.zip");
+          console.log("finished!")
       });
   }
 
